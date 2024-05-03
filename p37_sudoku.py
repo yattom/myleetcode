54ce116a2c3719ef3e7bee26fa153cd94434973a
yattom <tsutomu.yasui@gmail.com>
2024-05-03 10:10:34 +0900

    test to fix a cell

--- p37_sudoku.py
+++ p37_sudoku.py
@@ -5,3 +5,13 @@
 # - [ ] 行き詰まったら仮置きを戻す (バックトラック)
 # - [ ] 現在の状態を保持する
 
+
+def test_確定できる数字を置く():
+    # arrange
+    sudoku = Sudoku([[".", "8", "7", "6", "5", "4", "3", "2", "1"]])
+    # act
+    value = sudoku.fix_cell(0, 0)
+    # assert
+    assert value == "9"
+
+
