0443ee516efaa3e80629f75a7f1ea6041fcc58f1
yattom <tsutomu.yasui@gmail.com>
2024-05-03 10:24:44 +0900

    fix cell - case of non-fixed

--- p37_sudoku.py
+++ p37_sudoku.py
@@ -1,6 +1,11 @@
 # TODO
 # 数独を解く
 # - [ ] 確定できる数字を置く
+#   - [x] 横一行だけで確定できる数字を置く
+#   - [ ] 数字を確定できない場合
+#   - [ ] 縦一列だけで確定できる数字を置く
+#   - [ ] 3x3ブロックだけで確定できる数字を置く
+#   - [ ] 組み合わせで確定できる数字を置く
 # - [ ] 数字を仮置きして進める
 # - [ ] 行き詰まったら仮置きを戻す (バックトラック)
 # - [ ] 現在の状態を保持する
@@ -10,21 +15,41 @@ def test_確定できる数字を置く():
     # arrange
     sudoku = Sudoku([[".", "8", "7", "6", "5", "4", "3", "2", "1"]])
     # act
-    value = sudoku.fix_cell(0, 0)
+    value = sudoku.get_cell_value_if_fixed(0, 0)
     # assert
     assert value == "9"
+
+
+def test_確定できる数字を置く_2行目 ():
+    # arrange
+    sudoku = Sudoku([[], [".", "8", "7", "6", "5", "4", "3", "2", "1"]])
+    # act
+    value = sudoku.get_cell_value_if_fixed(0, 1)
+    # assert
+    assert value == "9"
+
+def test_確定できる数字を置く_数字を確定できない場合():
+    # arrange
+    sudoku = Sudoku([["."]])
+    # act
+    value = sudoku.get_cell_value_if_fixed(0, 0)
+    # assert
+    assert value == None
 
 
 class Sudoku:
     def __init__(self, cells: list[list[str]]):
         self.cells = cells
 
-    def fix_cell(self, col: int, row: int) -> str:
+    def get_cell_value_if_fixed(self, col: int, row: int) -> str | None:
         available = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         for c in self.cells[row]:
             if c == ".":
                 continue
             available.remove(c)
-        return available[0]
+        if len(available) == 1:
+            return available[0]
+        else:
+            return None
 
 
